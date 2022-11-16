a9b2c745c0925245258b2e69de24e17fa59f87cf
Neil Schark <github@schark.eu>
2022-11-16 19:56:20 +0100

    added pie chart

--- bullshitgraphs.py
+++ bullshitgraphs.py
@@ -7,6 +7,31 @@ from typing import List
 BASE_FILENAME="develop"
 OUTPUT_TYPE="png"
 
+def create_pie_chart(keywords: List[str], base_filename: str, output_type: str):
+    data = []
+    explode = []
+    biggest_value = 0
+    biggest_iterator = 0
+
+    for i, _ in enumerate(keywords):
+        random_value = random.randint(10, 100)
+        data.append(random_value)
+        explode.append(0)
+
+        if random_value >= biggest_value:
+            biggest_iterator = i
+            biggest_value = random_value
+
+    explode[biggest_iterator] = 0.1
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_xlabel("Distribution of value")
+    ax1.pie(data, explode=explode, labels=keywords, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.savefig(f"outputs/{base_filename}_pie.{output_type}")
+
 def create_bar_chart(keywords: List[str], base_filename: str, output_type: str):
     data = []
 
@@ -26,10 +51,11 @@ def main():
         if i == 0:
             continue
         keywords.append(element)
-            
+
     print(f"Your important {len(keywords)} keywords are: {keywords}")
 
     create_bar_chart(keywords, BASE_FILENAME, OUTPUT_TYPE)
+    create_pie_chart(keywords, BASE_FILENAME, OUTPUT_TYPE)
 
     print("Your important graphs were created")
 
